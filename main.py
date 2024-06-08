58be3095df8c578cbcd799521d9ffd91dc9d603f
patruiz <142975437+patruiz@users.noreply.github.com>
2024-06-07 23:53:59 -0700

    creating mesh data

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
+import time 
 import pandas as pd
 import pyvista as pv
 
-# Define the file path
 file_path = 'A_Hedge_02SEP2022.asc'
+print("Loading data...")
 
-# Read the ASC file with whitespace as the delimiter, skipping the first 6 rows
 ascii_grid = pd.read_csv(file_path, skiprows=6, delimiter = ',', header=None)
 
-# Define column names (assuming the first three columns are x, y, z coordinates)
 ascii_grid.columns = ['x', 'y', 'z', 'Column4', 'Column5', 'Column6']
+print("Creating point cloud...")
 
-# Create a point cloud
 point_cloud = pv.PolyData(ascii_grid[['x', 'y', 'z']].values)
+print("Generating mesh...")
 
-# Plot the point cloud
+start_time = time.time()
+mesh = point_cloud.delaunay_3d()
+end_time = time.time()
+print("Preparing plot...")
+
+mesh.save('test.vtk')
+print("Saving mesh...")
+
 plotter = pv.Plotter()
-plotter.add_points(point_cloud, scalars=ascii_grid['z'], cmap='viridis')
+# plotter.add_points(point_cloud, scalars=ascii_grid['z'], cmap='viridis')
+plotter.add_mesh(mesh, scalars=ascii_grid['z'], cmap='viridis', show_edges=True)
+plotter.add_axes(line_width=5, labels_off=True)
+print("Displaying plot...")
+
 plotter.show()
+
+
+print(f"Mesh generation time: {start_time - end_time}")
